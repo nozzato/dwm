d6edd88099776fe2195bce9750183cc47d4c430a
Noah Torrance <noahtorrance27@gmail.com>
2022-02-23 23:34:42 +0000

    Update status bar

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx         = 5;    /* gaps between windows */
 static const unsigned int snap          = 32;   /* snap pixel */
 static const int showbar                = 1;    /* 0 means no bar */
 static const int topbar                 = 1;    /* 0 means bottom bar */
-static const char *fonts[]              = { "monospace:size=13" };
-static const char dmenufont[]           = "monospace:size=13";
+static const char *fonts[]              = { "inconsolata:size=14" };
+static const char dmenufont[]           = "inconsolata:size=14";
 static const char col_gray1[]           = "#222222";
 static const char col_gray2[]           = "#444444";
 static const char col_gray3[]           = "#bbbbbb";
@@ -43,8 +43,8 @@ static const char *colors[][3] = {
 /* tagging */
 //static const char *tags[]         = { "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9"  };
 //static const char *tags[]         = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
-static const char *tags[]           = { "",  "",  "",  "",  "",  "",  "",  "",  ""  };
-static const char *defaulttagapps[] = { "st", "stranger", "firefox", "betterbird", "discord", "steam", "gimp", "lowriter", "code" };
+static const char *tags[]           = { "",  "",  "",  "",  "",  "",  "",  "",  "" };
+static const char *defaulttagapps[] = { "st", "firefox", "stranger", "vim", "libreoffice", "gimp", "betterbird", "discord", "steam" };
 
 static const Rule rules[] = {
     /* xprop(1):
