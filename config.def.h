cd19e74f6ad7372c3a3651012512a8dfe41f5cb9
Noah Torrance <noahtorrance27@gmail.com>
2022-08-30 20:05:42 +0100

    Add scratchpads patch

--- config.def.h
+++ config.def.h
@@ -18,6 +18,18 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+typedef struct {
+	const char *name;
+	const void *cmd;
+} Sp;
+const char *spcmd1[] = {"st", "-t", "scratchpad", "-n", "scratchpad", "-g", "120x34", NULL };
+const char *spcmd2[] = {"keepassxc", NULL };
+static Sp scratchpads[] = {
+	/* name          cmd  */
+	{"spterm",      spcmd1},
+	{"keepassxc",   spcmd2},
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
@@ -26,9 +38,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance      title       tags mask       isfloating   monitor */
+	{ "Gimp",	  NULL,			NULL,		0,				1,			 -1 },
+	{ NULL,		  "scratchpad",	NULL,		SPTAG(0),		1,			 -1 },
+	{ NULL,		  "keepassxc",	NULL,		SPTAG(1),		1,			 -1 },
 };
 
 /* layout(s) */
@@ -85,6 +98,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.ui = 0 } },
+	{ MODKEY,                       XK_x,      togglescratch,  {.ui = 1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
