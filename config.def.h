9342cf028f3cd222a0c086bad484f4416bdf15ca
Noah Torrance <noahtorrance27@gmail.com>
2022-08-31 03:32:53 +0100

    Change terminal to kitty

--- config.def.h
+++ config.def.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
