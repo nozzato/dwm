8b6e692cc0c5dd1e2ff394980db693d71532f56e
Noah Torrance <noahtorrance27@gmail.com>
2022-09-02 02:15:25 +0100

    Add doublepressquit patch

--- dwm.c
+++ dwm.c
@@ -27,6 +27,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -1533,7 +1534,24 @@ void
 quit(const Arg *arg)
 {
 	if(arg->i) restart = 1;
-	running = 0;
+	FILE *fd = NULL;
+	struct stat filestat;
+
+	if ((fd = fopen(lockfile, "r")) && stat(lockfile, &filestat) == 0) {
+		fclose(fd);
+
+		if (filestat.st_ctime <= time(NULL)-2)
+			remove(lockfile);
+	}
+
+	if ((fd = fopen(lockfile, "r")) != NULL) {
+		fclose(fd);
+		remove(lockfile);
+		running = 0;
+	} else {
+		if ((fd = fopen(lockfile, "a")) != NULL)
+			fclose(fd);
+	}
 
 	if (restart == 1)
 		saveSession();
