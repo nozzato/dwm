90b9a2c9ab48f7aa459a8931c7fd2213ec6abe30
Noah Torrance <noahtorrance27@gmail.com>
2022-10-26 02:37:27 +0100

    Attach new windows at the bottom

--- config.def.h
+++ config.def.h
@@ -66,7 +66,7 @@ static const float mfact         = 0.55; /* factor of master area size [0.05..0.
 static const int nmaster         = 1;    /* number of clients in master area */
 static const int resizehints     = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen  = 1;    /* 1 will force focus on the fullscreen window */
-static const int attachdirection = 5;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
+static const int attachdirection = 4;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
