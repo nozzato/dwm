8dec1c29bc4e6e416ceb52516c8700f2abc8194c
Noah Torrance <noahtorrance27@gmail.com>
2022-10-30 22:13:03 +0000

    Add MobyDroid rule

--- config.def.h
+++ config.def.h
@@ -48,6 +48,7 @@ static const Rule rules[] = {
 	/* class         instance  title                              tags mask  isfloating  monitor */
 	{ "Gimp",        NULL,     NULL,                              0,         1,          -1 },
 	{ "Gimp",        NULL,     "GNU Image Manipulation Program",  0,         0,          -1 },
+    { "MobyDroid",   NULL,     "Working...",                      0,         1,          -1 },
 	{ "ncmpcppt",    NULL,     NULL,                              1 << 4,    0,          -1 },
 	{ "thunderbird", NULL,     NULL,                              1 << 5,    0,          -1 },
 	{ "discord",     NULL,     NULL,                              1 << 6,    0,          -1 },
