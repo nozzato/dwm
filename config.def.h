4e047cd37e1e64a3428f5aef03815609443b5c04
Noah Torrance <noahtorrance27@gmail.com>
2023-05-02 10:38:24 +0100

    Replace icons

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVuSansMono Nerd Font:size=10", "Font Awesome 6 Free Solid:size=10", "Blobmoji:size=10" };
+static const char *fonts[]          = { "DejaVu Sans Mono:size=10", "Blobmoji:size=10", "Font Awesome 6 Free Solid:size=10" };
 static const char *colors[][3]      = {
 	/*                fg         bg         border   */
 	[SchemeNorm]  = { "#a9b1d6", "#121116", "#292e42" },
